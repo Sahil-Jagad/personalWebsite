136786bd3af797ba7652c7768581f7892735066a
Sahil Jagad <sjagad@uci.edu>
2021-03-11 15:54:33 -0800

    GROC query + schema for Projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,21 @@
-import React from "react"
-
+import React, {useEffect, useState} from "react";
+import sanityClient from "../client.js"
+import { Link } from "react-router-dom";
 export default function Project() {
+    const [projectData, setProjectData] = useState(null);
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_types == "project]{
+            title,
+            date,
+            place,
+            description,
+            projectType,
+            link,
+            tags
+        }`).then((data) => setProjectData(data))
+        .catch(console.error);
+    }, [])
+
     return <h1>Project Page</h1>
 }
