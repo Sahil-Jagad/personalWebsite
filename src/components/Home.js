2fa24333c533a92650a138effb190af5e9edf44e
Sahil Jagad <sjagad@uci.edu>
2021-03-10 23:16:29 -0800

    updated homepage pic

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,18 @@
 import React from "react"
-//import image from "../media/goldensunset.jpg"
-//import image2 from "../media/RockyGoldenGate.jpg"
-import image3 from "../media/mont.jpg"
+import image from "../media/forest2.jpg"
+
+
 
 export default function Home() {
     return (
         <main>
             <img 
-                src={image3} 
+                src={image} 
                 alt="Golden Sunset" 
                 className="absolute object-cover w-full h-full"
             />
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hi. I'm Sahil.</h1>
+                <h1 className="text-6xl text-green-50 font-bold cursive leading-none lg:leading-snug home-name">Hi. I'm Sahil.</h1>
             </section>
         </main>
     )
