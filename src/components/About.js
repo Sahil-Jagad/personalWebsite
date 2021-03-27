1d6aa1aee989900b8c1a7a8754a8f5375e180f4c
Sahil Jagad <sjagad@uci.edu>
2021-03-26 23:10:51 -0700

    blog post page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,13 @@
-import React from "react";
+import React, {userEffect, useState} from "react";
+import sanityClient from "../client.js"
+
 
 export default function About() {
+
+    const [author, setAuthor] = useState(null);
+
+
+
+
     return <h1>About Page</h1>
 }
